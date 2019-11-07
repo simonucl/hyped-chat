9ba72d6df2593681f12083bbce5146b6a27d674d
simonucl <55485491+simonucl@users.noreply.github.com>
2019-11-07 19:22:19 +0000

    Add files via upload

--- clientGUI.py
+++ clientGUI.py
@@ -8,10 +8,11 @@ SERVER_IP   = '127.0.0.1'
 PORT_NUMBER = 5000
 SIZE = 1024
 mySocket = socket( AF_INET, SOCK_DGRAM )
+
 class IntroWindow:
     def __init__(self, intro):
         self.intro = intro
-        intro.geometry("550x500")
+        intro.geometry("1920x720")
         self.frame = tk.Frame(intro)
         self.frame.pack()
 
@@ -19,24 +20,29 @@ class IntroWindow:
         greeting = "         Hi, there!I am the client.\nHow's it going?\n      Let's Chat!\n"
 
 
-        photo = tk.PhotoImage(file="test2.gif")
+        photo = tk.PhotoImage(file="rsz_test.gif")
 
-        self.config = tk.Label(intro, compound=tk.CENTER,
-                               text=greeting, font=("Arial", 23), image=photo)
+        #self.config = tk.Label(intro, compound=tk.CENTER,
+                               #text=greeting, font=("Arial", 23), image=photo)
+        self.config = tk.Label(intro, compound=tk.CENTER, image=photo)                       
         self.config.image = photo
-        self.config.pack()
+        self.config.place(x=570, y=200)
+
+        self.greet = tk.Label(intro, compound=tk.CENTER, text=greeting, font=("Arial",30))
+        self.greet.place(x=500, y=400)
 
         self.text_btn = tk.Button(intro, text="Get Connected", command=self.connect)
-        self.text_btn.pack()
+        self.text_btn.place(x=670, y=600)
 
         self.close_btn = tk.Button(intro, text="Quit", command=intro.quit)
-        self.close_btn.pack()
+        self.close_btn.place(x=705, y=630)
 
     def connect(self):
         self.intro.destroy()
         chatting = tk.Tk()
         chatting.title("ChatRoom")
         Chat(chatting)
+        
 
 
 class Chat:
@@ -75,15 +81,15 @@ class Chat:
         i = 0
         x=[]
         if msg == "send data" :
-            while i < 50:
-                x.append(random.randint(0,1))
+            while i < 500:
+                x.append(random.uniform(0,1))
     # mySocket.sendto(myMessage.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
                 mySocket.sendto(str(sum(x)).encode('utf-8'),(SERVER_IP,PORT_NUMBER))
     # mySocket.sendto(str(i).encode('utf-8'),(SERVER_IP,PORT_NUMBER))
                 self.msg_list.insert(tk.END, "Velocity:"+str(sum(x))+"   Time:"+str(i))
                 i = i + 1
-            while 50<=i<100:
-                x.append(random.randint(0,10))
+            while 500<=i<=1000:
+                x.append(random.uniform(0,10))
                 mySocket.sendto(str(sum(x)).encode('utf-8'),(SERVER_IP,PORT_NUMBER))
                 self.msg_list.insert(tk.END, "Velocity:"+str(sum(x))+"   Time:"+str(i))
                 i = i+1
